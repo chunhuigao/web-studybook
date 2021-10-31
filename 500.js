ecf5da63246f4fb74d2c5e20fa963082945eeea9
北斗 <1912128546@qq.com>
2021-10-31 11:24:47 +0800

    ✨ feat:leetcode第500题ac

---
+++ 500.js
@@ -0,0 +1,35 @@
+;`
+给你一个字符串数组 words ，只返回可以使用在 美式键盘 同一行的字母打印出来的单词。键盘如下图所示。
+第一行由字符 "qwertyuiop" 组成。
+第二行由字符 "asdfghjkl" 组成。
+第三行由字符 "zxcvbnm" 组成。`
+
+var findWords = function (words) {
+  let l = words.length
+  let arr = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+  let result = []
+  for (let i = 0; i < l; i++) {
+    str = words[i].toLowerCase()
+    let h = words[i].length
+    let index = 0
+    if (arr[0].indexOf(str[0]) != -1) {
+      index = 0
+    } else if (arr[1].indexOf(str[0]) != -1) {
+      index = 1
+    } else if (arr[2].indexOf(str[0]) != -1) {
+      index = 2
+    }
+    let tag = true
+
+    for (let k = 0; k < h; k++) {
+      if (arr[index].indexOf(str[k]) == -1) {
+        tag = false
+        break
+      }
+    }
+    if (tag) {
+      result.push(words[i])
+    }
+  }
+  return result
+}

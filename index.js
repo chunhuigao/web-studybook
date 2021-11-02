4054ee3c96d80bd3737721edb82519d2f09d28e4
北斗 <1912128546@qq.com>
2021-11-02 23:00:33 +0800

    ✨ feat:tes疑问

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const list1 = JSON.parse(readline()||"[]")
-const list2 = JSON.parse(readline()||"[]")
- console.log('',list1,list2)
+const s1 = readline()
+const s2 = readline()
+const list1 = JSON.parse(s1 || '[]')
+const list2 = JSON.parse(s2 || '[]')
+
 let node1 = arrayToNodeList(list1)
 let node2 = arrayToNodeList(list2)
 
 const result = []
 while (node1 || node2) {
- 
   if (node1 && node2) {
     if (node1.val > node2.val) {
       result.push(node2.val)
       node2 = node2.next
     } else {
-      result.push(node1.val);
+      result.push(node1.val)
       node1 = node1.next
     }
-  }else{
-     if(node1){
-        result.push(node1.val);
-        node1 = node1.next
-      }
-      if(node2){
-        result.push(node2.val);
-        node2 = node2.next
-      }
+  } else {
+    if (node1) {
+      result.push(node1.val)
+      node1 = node1.next
+    }
+    if (node2) {
+      result.push(node2.val)
+      node2 = node2.next
+    }
   }
- 
 }
 
-const res = arrayToNodeList(result) 
-console.log('1111',res,result)
+const res = arrayToNodeList(result)
+
+console.log(s1, s2, res)
+
 // 链表构造函数
-function NodeList(val) {
+function ListNode(val) {
   this.val = val
   this.next = null
 }
 
 //数组转链表
 function arrayToNodeList(list) {
-  let header = new NodeList(0)
+  let header = new ListNode(0)
   let current = header
   for (let i = 0; i < list.length; i++) {
-    current.next = { val: list[i], next: null }
+    current.next = { val: Number(list[i]), next: null }
     current = current.next
   }
   return header.next

a2edce51eba152155b058bc139e43cb65cef183d
北斗 <1912128546@qq.com>
2021-11-03 20:36:17 +0800

    🌈 style:修改文件目录

--- index2.js
+++ index2.js
@@ -1,24 +1,20 @@
-const s = '{(}'
-const len = s.length
-let sign = true
-const arr = []
+var mergeTwoLists = function (l1, l2) {
+  const prehead = new ListNode(-1)
 
-for (let i = 0; i < len; i++) {
-  if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
-    arr.push(s[i])
+  let prev = prehead
+  while (l1 != null && l2 != null) {
+    if (l1.val <= l2.val) {
+      prev.next = l1
+      l1 = l1.next
+    } else {
+      prev.next = l2
+      l2 = l2.next
+    }
+    prev = prev.next
   }
-  if (s[i] === ')') {
-    const x = arr.pop()
-    if (x !== '(') console.log(false)
-  }
-  if (s[i] === ']') {
-    const x = arr.pop()
-    if (x !== '[') console.log(false)
-  }
-  if (s[i] === '}') {
-    const x = arr.pop()
-    if (x !== '{') console.log(false)
-  }
+
+  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+  prev.next = l1 === null ? l2 : l1
+
+  return prehead.next
 }
-
-console.log(arr.length === 0)

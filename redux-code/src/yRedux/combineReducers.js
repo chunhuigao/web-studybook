89bdeff4331d94a341133e1bc835a401e219af1a
北斗 <1912128546@qq.com>
2021-11-09 22:52:30 +0800

    ✨ feat:完成redux的combineReducers方法

---
+++ redux-code/src/yRedux/combineReducers.js
@@ -0,0 +1,16 @@
+// 返回reducer
+
+export default function combineReducers(reducers) {
+  return function combintion(state = {}, action) {
+    let nextState = {}
+    let hasChange = false
+    for (let key in reducers) {
+      const reducer = reducers[key]
+      nextState[key] = reducer(state[key], action)
+      hasChange = hasChange || nextState[key] !== state[key]
+    }
+    hasChange =
+      hasChange || Object.keys(nextState).length !== Object.keys(state).length
+    return hasChange ? nextState : state
+  }
+}

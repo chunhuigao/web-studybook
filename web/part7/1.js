b143640c63a5e4c195bd7c332d86fec2e4f1fded
北斗 <1912128546@qq.com>
2021-11-12 22:33:34 +0800

    ✨ feat:出现前K个高频数

--- web/part7/1.js
+++ web/part7/1.js
@@ -1,50 +1,36 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
 const topKFrequent = (nums, k) => {
-  // 1. 设置哈希映射
-  const keyValue = {}
+  const map = {}
+  nums.forEach((n) => {
+    map[n] = (map[n] || 0) + 1
+  })
 
-  // 2. 遍历数组统计每个数字出现的次数
-  for (let i = 0; i < nums.length; i++) {
-    if (!keyValue[nums[i]]) {
-      keyValue[nums[i]] = 0
-    }
-    keyValue[nums[i]]++
+  // 先统计出现的次数,如果数据量少于k返回
+
+  if (Object.keys(map).length <= k) {
+    return Object.keys(map)
   }
 
-  // 3. 如果元素数量小于等于 k，直接返回
-  if (Object.keys(keyValue).length <= k) {
-    return Object.keys(keyValue)
-  }
+  // 数据量超过k呢？
 
-  // 4. 如果大于 k，利用桶排序
-  const bucket = []
-  // 4.1 遍历前面存储的哈希映射
-  for (let key in keyValue) {
-    // 4.2 如果该桶不存在，那就设置为空
-    if (!bucket[keyValue[key]]) {
-      bucket[keyValue[key]] = []
-    }
-    // 4.3 往桶里添加，例如出现频率为 2 的，有 4 和 2，那就塞里面
-    bucket[keyValue[key]].push(Number(key))
-  }
+  // 找个桶,桶最长为k;
+  const list = []
 
-  // 5. 统计结果
+  //遍历数据
+  Object.keys(map).forEach((k) => {
+    const idx = map[k]
+    list[idx] = (list[idx] || []).concat(k)
+  })
+
   const result = []
-  // 5.1 遍历桶，终止条件是 result 的长度小于 k
-  for (let i = bucket.length - 1; i >= 0, result.length < k; i--) {
-    // 5.1 如果该位置存在桶
-    if (bucket[i]) {
-      // 5.2 那么将桶里的东西提取出来放入结果
-      for (let j = 0; j < bucket[i].length; j++) {
-        result.push(bucket[i][j])
-      }
+  for (let i = list.length - 1; i >= 0; i--) {
+    if (result.length === k) return result.map(Number)
+    if (list[i]) {
+      result.push(...list[i])
     }
   }
-
-  // 6. 返回结果
-  return result
+  console.log('list', list)
 }
+var nums = [1, 1, 1, 2, 2, 3],
+  k = 2
+const ss = topKFrequent(nums, k)
+console.log(ss)

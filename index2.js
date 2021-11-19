24169aedac4e3a4d78c9d16c56783303c3fc5b8b
北斗 <1912128546@qq.com>
2021-11-19 21:08:17 +0800

    ✨ feat:添加shelljs依赖

--- index2.js
+++ index2.js
@@ -1,20 +1,13 @@
-var mergeTwoLists = function (l1, l2) {
-  const prehead = new ListNode(-1)
-
-  let prev = prehead
-  while (l1 != null && l2 != null) {
-    if (l1.val <= l2.val) {
-      prev.next = l1
-      l1 = l1.next
-    } else {
-      prev.next = l2
-      l2 = l2.next
-    }
-    prev = prev.next
-  }
-
-  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
-  prev.next = l1 === null ? l2 : l1
-
-  return prehead.next
+var shell = require('shelljs')
+//判定git命令是否可用
+if (!shell.which('git')) {
+  //向命令行打印git命令不可用的提示信息
+  shell.echo('Sorry, this script requires git')
+  //退出当前进程
+  shell.exit(1)
 }
+
+shell.exec('git add index.js')
+shell.exec('git commit -m "使用nodejs自动执行git"')
+shell.exec('git push origin master')
+shell.exit(1)

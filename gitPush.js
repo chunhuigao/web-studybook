7e349aa74ed7e8463694e96830669178d66d76a9
北斗 <1912128546@qq.com>
2021-11-19 21:02:51 +0800

    ✨ feat:修改文件名

--- gitPush.js
+++ gitPush.js
@@ -9,16 +9,11 @@ if (!shell.which('git')) {
 
 // shell.exec('git add index.js')
 // shell.exec('git commit -m "使用nodejs自动执行git"')
-const start()=>{
-  shell.exec('git push origin master', function (error, response) {
-    if (error) {
-      console.log('error', error)
-      return
-    }
-    console.log('response',response)
-   
-  })
-  shell.exit(1)
+function start() {
+  var code = shell.exec('git push origin master').code
+  if (code) {
+    console.log('code', code)
+    shell.exit(1)
+  }
 }
 start()
-

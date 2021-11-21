775c3b540fe46cf790ca54dcc6932e8b0d539df1
北斗 <1912128546@qq.com>
2021-11-21 13:08:49 +0800

    ✨ feat:编辑node文件，监听指定文件夹并将日期命名的json自动提交到git

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+function checkDateFormat(s) {
+  let string = s
+  if (
+    /^\d{4}-\d{2}-\d{2}$/.test(s) ||
+    /^\d{4}.\d{2}.\d{2}$/.test(s) ||
+    /^\d{4}_\d{2}_\d{2}$/.test(s)
+  ) {
+    string = s.replace(/[._-]/g, '')
+  }
+  if (string.length === 8) {
+    const year = string.substring(0, 4)
+    const month = string.substring(4, 6)
+    const day = string.substring(6, 8)
+    const current = new Date(`${year}.${month}.${day}`)
+    return current.toString() === 'Invalid Date'
+  }
+  return false
+}
+const path = require('path')
+console.log('path.resolve(__dirname)', path.resolve(__dirname, 'github-db'))

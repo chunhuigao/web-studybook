6e64eace1b7fbb0b8f9f6b79fad2158feb2dde4b
北斗 <1912128546@qq.com>
2021-11-23 20:33:33 +0800

    ✨ feat:二叉树后序遍历

---
+++ web/part10/145.js
@@ -0,0 +1,12 @@
+//二叉树的后序遍历
+var postorderTraversal = function (root) {
+  let result = []
+  helper(root)
+  return result
+  function helper(node) {
+    if (node === null) return
+    helper(node.left)
+    helper(node.right)
+    result.push(node.val)
+  }
+}

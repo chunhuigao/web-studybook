a51d8bf0263487bdbdd0ee64b363313e0106b434
北斗 <1912128546@qq.com>
2021-11-23 20:48:43 +0800

    ✨ feat:验证二叉树的前序序列化

---
+++ web/part10/331.js
@@ -0,0 +1,17 @@
+
+var isValidSerialization = function (preorder) {
+  const array = preorder.split(',')
+  const len = array.length;
+  const stack = []
+  for (let i = 0; i < len; i++) {
+    stack.push(array[i])
+    const sLen = stack.length-1
+    while(stack.length >=3  && stack[sLen] === stack[sLen-1] && stack[sLen] === '#' && stack[sLen-2] !== #){
+      stack.pop();
+      stack.pop();
+      stack.pop();
+      stack.push('#')
+    }
+  }
+  return stack.length === 1 && stack.pop() === '#'
+}

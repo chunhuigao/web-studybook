3667b812a02699a7f3a29968cc3d041c7c6427d7
北斗 <1912128546@qq.com>
2021-11-26 21:34:13 +0800

    ✨ feat:最近的请求次数

---
+++ web/part12/933.js
@@ -0,0 +1,12 @@
+var RecentCounter = function () {
+  this.queue = []
+}
+
+RecentCounter.prototype.ping = function (t) {
+  this.queue.push(t)
+  const time = t - 3000
+  while (this.queue[0] < time) {
+    this.queue.shift()
+  }
+  return this.queue.length
+}

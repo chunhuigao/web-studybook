97fcf204c2b61a1d95e2d4007fb5cbbaa1f4cb1c
北斗 <1912128546@qq.com>
2021-11-28 00:06:30 +0800

    ✨ feat:完善测试代码

--- leetcode/519.js
+++ leetcode/519.js
@@ -12,11 +12,12 @@ var Solution = function (m, n) {
 /**
  * @return {number[]}
  */
-Solution.prototype.flip = function () {
-  const x = Math.floor(Math.random() * this.total)
+Solution.prototype.flip = function (index) {
+  const key = [8, 8, 4, 7, 7][index]
   this.total--
-  const idx = this.map.get(x) || x
-  this.map.set(x, this.map.get(this.total) || this.total)
+  const idx = this.map.get(key) || key
+  this.map.set(key, this.map.get(this.total) || this.total)
+  console.log('map', key, this.map)
   return [Math.floor(idx / this.n), idx % this.n]
 }
 
@@ -26,4 +27,11 @@ Solution.prototype.flip = function () {
 Solution.prototype.reset = function () {
   this.total = this.m * this.n
   this.map.clear()
+}
+
+var obj = new Solution(3, 5)
+
+for (let i = 0; i < 5; i++) {
+  var param_1 = obj.flip(i)
+  console.log(param_1)
 }

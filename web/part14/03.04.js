2f758414317efee60ed05feb0e88510e7e67314b
北斗 <1912128546@qq.com>
2021-12-02 21:27:59 +0800

    ✨ feat:优化代码

--- web/part14/03.04.js
+++ web/part14/03.04.js
@@ -1,38 +1,21 @@
-/**
- * Initialize your data structure here.
- */
-var MyQueue = function () {}
-
-/**
- * Push element x to the back of queue.
- * @param {number} x
- * @return {void}
- */
-MyQueue.prototype.push = function (x) {}
-
-/**
- * Removes the element from in front of queue and returns that element.
- * @return {number}
- */
-MyQueue.prototype.pop = function () {}
-
-/**
- * Get the front element.
- * @return {number}
- */
-MyQueue.prototype.peek = function () {}
-
-/**
- * Returns whether the queue is empty.
- * @return {boolean}
- */
-MyQueue.prototype.empty = function () {}
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * var obj = new MyQueue()
- * obj.push(x)
- * var param_2 = obj.pop()
- * var param_3 = obj.peek()
- * var param_4 = obj.empty()
- */
+class MyQueue {
+  constructor() {
+    this.left = []
+    this.right = []
+  }
+  push(x) {
+    this.left.push(x)
+  }
+  pop() {
+    while (this.left.length) this.right.push(this.left.pop())
+    const result = this.right.pop()
+    while (this.right.length) this.left.push(this.right.pop())
+    return result
+  }
+  peek() {
+    return this.left[0]
+  }
+  empty() {
+    return !this.left.length
+  }
+}

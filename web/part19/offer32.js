f4d33725950f9638bcbe13537743bb3223c1980c
北斗 <1912128546@qq.com>
2021-12-18 17:14:02 +0800

    ✨ feat:迭代求解

--- web/part19/offer32.js
+++ web/part19/offer32.js
@@ -23,3 +23,20 @@ var levelOrder = function (root) {
     helper(node.right, level + 1)
   }
 }
+
+var levelOrder = function (root) {
+  if (root === null) return []
+  const result = []
+  let stack = [root]
+  while (stack.length) {
+    const path = []
+    for (let i = stack.length - 1; i >= 0; i--) {
+      let temp = stack.shift()
+      path.push(temp.val)
+      temp.left && stack.push(temp.left)
+      temp.right && stack.push(temp.right)
+    }
+    result.push(path)
+  }
+  return result
+}

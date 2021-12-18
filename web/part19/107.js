5690835bd1ff03c20e97171a49e9fcd5377fb16f
北斗 <1912128546@qq.com>
2021-12-18 19:21:05 +0800

    ✨ feat:107. 二叉树的层序遍历 II

--- web/part19/107.js
+++ web/part19/107.js
@@ -7,9 +7,28 @@ var levelOrderBottom = function (root) {
     if (node === null) return
     if (result[level] === undefined) {
       result[level] = [node.val]
-    } else [result[level].push(node.val)]
+    } else {
+      result[level].push(node.val)
+    }
 
     helper(node.left, level + 1)
     helper(node.right, level + 1)
   }
+}
+
+var levelOrderBottom = function (root) {
+  if (root === null) return []
+  const result = []
+  let stack = [root]
+  while (stack.length) {
+    const path = []
+    for (let i = stack.length - 1; i >= 0; i--) {
+      let temp = stack.shift()
+      path.push(temp.val)
+      temp.left && stack.push(temp.left)
+      temp.right && stack.push(temp.right)
+    }
+    result.unshift(path)
+  }
+  return result
 }

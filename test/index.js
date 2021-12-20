d561025c8f9633e06f92d8d038b48546aec9c20d
北斗 <1912128546@qq.com>
2021-12-20 10:01:45 +0800

    ✨ feat:借用快排思想解决问题

--- test/index.js
+++ test/index.js
@@ -1,1 +1,34 @@
-// node 监听文件夹
+var getLeastNumbers = function (arr, k) {
+  // 快速排序方法
+  const len = arr.length
+  let left = 0
+  let right = len - 1
+  let idx = partition(arr, left, right)
+  // while (idx !== k) {
+  //   if (idx < k) {
+  //     left = idx + 1
+  //   } else {
+  //     right = idx - 1
+  //   }
+  //   idx = partition(arr, left, right)
+  // }
+  // return arr.slice(0, k)
+  console.log('arr', arr)
+  function partition(arr, lo, hi) {
+    let pivot = arr[lo]
+    let index = lo
+    for (let i = lo; i <= hi; i++) {
+      if (arr[i] < pivot) {
+        index++
+        ;[arr[i], arr[index]] = [arr[index], arr[i]]
+      }
+    }
+    ;[arr[lo], arr[index]] = [arr[index], arr[lo]]
+    return index
+  }
+}
+
+var arr = [2, 8, 1, 1, 0, 11, -1, 0],
+  k = 2
+const aa = getLeastNumbers(arr, k)
+console.log(aa)
--- web/part20/offer40.js
+++ web/part20/offer40.js
@@ -2,3 +2,32 @@ var getLeastNumbers = function (arr, k) {
   arr.sort((a, b) => a - b)
   return arr.slice(0, k)
 }
+
+var getLeastNumbers = function (arr, k) {
+  // 快速排序方法
+  const len = arr.length
+  let left = 0
+  let right = len - 1
+  let idx = partition(arr, left, right)
+  while (idx !== k) {
+    if (idx < k) {
+      left = idx + 1
+    } else {
+      right = idx - 1
+    }
+    idx = partition(arr, left, right)
+  }
+  return arr.slice(0, k)
+  function partition(arr, start, end) {
+    let pivot = arr[start]
+    let index = start
+    for (let i = start; i <= end; i++) {
+      if (arr[i] < pivot) {
+        index++
+        ;[arr[i], arr[index]] = [arr[index], arr[i]]
+      }
+    }
+    ;[arr[start], arr[index]] = [arr[index], arr[start]]
+    return index
+  }
+}

fa3854b0d529a0ceeaf6245b9fafb1097086a5d4
北斗 <1912128546@qq.com>
2021-12-21 21:56:05 +0800

    ✨ feat:添加注释

--- web/part21/355.js
+++ web/part21/355.js
@@ -1,18 +1,9 @@
-/**
- * Initialize your data structure here.
- */
 var Twitter = function () {
   this.users = {}
   this.userAndMess = {}
   this.index = 0
 }
 
-/**
- * Compose a new tweet.
- * @param {number} userId
- * @param {number} tweetId
- * @return {void}
- */
 Twitter.prototype.postTweet = function (userId, tweetId) {
   if (this.users[userId] === undefined) {
     this.users[userId] = [userId]
@@ -26,11 +17,6 @@ Twitter.prototype.postTweet = function (userId, tweetId) {
   }
 }
 
-/**
- * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
- * @param {number} userId
- * @return {number[]}
- */
 Twitter.prototype.getNewsFeed = function (userId) {
   const array = this.users[userId]
   if (Boolean(array)) {
@@ -50,12 +36,6 @@ Twitter.prototype.getNewsFeed = function (userId) {
   }
 }
 
-/**
- * Follower follows a followee. If the operation is invalid, it should be a no-op.
- * @param {number} followerId
- * @param {number} followeeId
- * @return {void}
- */
 Twitter.prototype.follow = function (followerId, followeeId) {
   if (followerId === followeeId) return
   if (this.users[followerId] === undefined) {
@@ -68,12 +48,6 @@ Twitter.prototype.follow = function (followerId, followeeId) {
   }
 }
 
-/**
- * Follower unfollows a followee. If the operation is invalid, it should be a no-op.
- * @param {number} followerId
- * @param {number} followeeId
- * @return {void}
- */
 Twitter.prototype.unfollow = function (followerId, followeeId) {
   if (followerId === followeeId) return
   if (this.users[followerId] === undefined) {
@@ -85,12 +59,3 @@ Twitter.prototype.unfollow = function (followerId, followeeId) {
     }
   }
 }
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * var obj = new Twitter()
- * obj.postTweet(userId,tweetId)
- * var param_2 = obj.getNewsFeed(userId)
- * obj.follow(followerId,followeeId)
- * obj.unfollow(followerId,followeeId)
- */

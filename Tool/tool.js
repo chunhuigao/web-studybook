3d1abb887f8268d2ac708378589fb7a5196069b4
北斗 <1912128546@qq.com>
2021-12-30 22:24:22 +0800

    ✨ feat:完善判断一个变量是否是一个空对象方法

--- Tool/tool.js
+++ Tool/tool.js
@@ -1,7 +1,17 @@
 const isEmptyObject = (o) => {
   if (typeof o === 'object') {
-    return Object.keys(obj).length === 0
+    return (
+      Object.getOwnPropertyNames(o).length === 0 &&
+      Object.getOwnPropertySymbols(o).length === 0
+    )
   } else {
     return false
   }
 }
+const k1 = Symbol()
+const k2 = '1'
+var a = {}
+var b = {}
+b[k1] = 1
+
+console.log(isEmptyObject(a))

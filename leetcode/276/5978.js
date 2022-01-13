54877e4f51f8b9051738ee368c678bc4dd3d7b60
北斗 <1912128546@qq.com>
2022-01-13 23:40:48 +0800

    ✨ feat:leetcode周赛第3题

--- leetcode/276/5978.js
+++ leetcode/276/5978.js
@@ -18,7 +18,51 @@ var wordCount = function (startWords, targetWords) {
   })
   return result
 }
-var startWords = ['uh'],
-  targetWords = ['uhr', 'u', 'r']
+
+/**
+ * @param {string[]} startWords
+ * @param {string[]} targetWords
+ * @return {number}
+ */
+
+var wordCount = function (startWords, targetWords) {
+  const set = new Set()
+  const getIndex = (s) => s.charCodeAt() - 'a'.charCodeAt()
+
+  startWords.forEach((word) => {
+    let num = 0
+    for (let i = 0; i < word.length; i++) {
+      num |= 1 << getIndex(word[i])
+    }
+    set.add(num)
+  })
+  console.log('set', set)
+  // a b d
+  // a b c d
+
+  let result = 0
+
+  for (const target of targetWords) {
+    let x = 0
+    for (let i = 0; i < target.length; i++) {
+      x |= 1 << getIndex(target[i])
+    }
+
+    for (let i = 0; i < target.length; i++) {
+      let t = 1 << getIndex(target[i])
+     
+      const k = x ^ t
+      if (set.has(k)) {
+        result++
+        break
+      }
+    }
+  }
+
+  return result
+}
+
+var startWords = ['ac', 'a'],
+  targetWords = ['acd', 'ab', 'a']
 const aa = wordCount(startWords, targetWords)
 console.log(aa)

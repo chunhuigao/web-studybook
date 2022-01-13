9c5eb681ef7b5fbd776970ad4f2b6a0a000f5718
北斗 <1912128546@qq.com>
2022-01-13 23:41:34 +0800

    ✨ feat:新增类型判断函数

--- Tool/tool.js
+++ Tool/tool.js
@@ -8,6 +8,20 @@ const isEmptyObject = (o) => {
     return false
   }
 }
+
+var isType = function (type) {
+  return function (obj) {
+    console.log(
+      'Object.prototype.toString.call(obj)',
+      Object.prototype.toString.call(obj)
+    )
+    return Object.prototype.toString.call(obj) == '[object ' + type + ']'
+  }
+}
+const yinyin = isType('Array')([])
+const yinyin2 = isType('Object')({})
+console.log('yinyin', yinyin)
+console.log('yinyin', yinyin2)
 const k1 = Symbol()
 const k2 = '1'
 var a = {}

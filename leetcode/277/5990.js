c2ff67371a2ba4ca3b92f542ba1b1897b55f56ed
北斗 <1912128546@qq.com>
2022-01-23 22:15:04 +0800

    ✨ feat:5990. 找出数组中的所有孤独数字

--- leetcode/277/5990.js
+++ leetcode/277/5990.js
@@ -1,33 +1,22 @@
-var maximumGood = function (statements) {
-  let result = 0
-  const len = statements.length
-  const end = 1 << len
-  for (let i = 0; i < end; i++) {
-    let sign = true
-    for (let j = 0; j < len; j++) {
-      const t = i >> j
-      if (t & 1) {
-        for (let k = 0; k < statements[j].length; k++) {
-          if (statements[j][k] === 2) continue
-          if (
-            (statements[j][k] == 1 && ((i >> k) & 1) == 0) ||
-            (statements[j][k] == 0 && ((i >> k) & 1) == 1)
-          ) {
-            sign = false
-            break
-          }
-        }
-      }
-    }
-    let max = 0
-    if (sign) {
-      for (let j = 0; j < len; j++) {
-        if (i & (1 << j)) max += 1
-      }
-
-      result = Math.max(result, max)
-    }
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var findLonely = function(nums) {
+  const map = {};
+  for(let i = 0 ; i < nums.length ; i++){
+      const k = nums[i]
+      map[k] = (map[k] || 0) +  1;
   }
-
+  const result = []
+  for(let i = 0 ; i < nums.length ; i++){
+      const k = nums[i] 
+      const k1 = nums[i] + 1;
+       const k2 = nums[i] - 1;
+      if(!map[k1] && !map[k2] && map[k] === 1){
+          result.push(k)
+      }
+  }
   return result
-}
+
+};

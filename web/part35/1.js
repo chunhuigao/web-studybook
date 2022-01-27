eedcfe84fa6b8d5c553539fe7d470ccb0ea13e96
北斗 <1912128546@qq.com>
2022-01-27 20:38:56 +0800

    ✨ feat:两数之和

--- web/part35/1.js
+++ web/part35/1.js
@@ -7,3 +7,17 @@ var twoSum = function (nums, target) {
     }
   }
 }
+
+var twoSum = function (nums, target) {
+  const map = {}
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i]
+    const k = target - n
+    if (map[n] === undefined) {
+      map[k] = i
+    } else {
+      return [i, map[n]]
+    }
+  }
+  return []
+}

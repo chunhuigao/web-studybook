052003505f8eca344a369c6550d56ffcea45956b
北斗 <1912128546@qq.com>
2022-02-02 14:22:00 +0800

    ✨ feat:2155. 分组得分最高的所有下标

---
+++ leetcode/278/2155.js
@@ -0,0 +1,24 @@
+var maxScoreIndices = function (nums) {
+  let one = 0
+  const len = nums.length
+  for (let i = 0; i < len; i++) {
+    if (nums[i] === 1) one++
+  }
+  let map = {}
+  let max = one
+  map[one] = [0]
+  let zero = 0
+  for (let i = 1; i <= len; i++) {
+    if (nums[i - 1] === 0) zero++
+    if (nums[i - 1] === 1) one--
+    const k = zero + one
+
+    max = Math.max(max, k)
+    if (map[k] === undefined) {
+      map[k] = [i]
+    } else {
+      map[k].push(i)
+    }
+  }
+  return map[max]
+}

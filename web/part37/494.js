875e9d78d00283fadbf0cde92c51a45e4acf843f
北斗 <1912128546@qq.com>
2022-02-08 20:21:53 +0800

    ✨ feat:494. 目标和

--- web/part37/494.js
+++ web/part37/494.js
@@ -1,19 +1,32 @@
+// f[i][j]=f[i−1][j−nums[i−1]]+f[i−1][j+nums[i−1]]
 var findTargetSumWays = function (nums, S) {
-  let result = 0
-  dfs(0, 0)
-  return result
-  function dfs(idx, t) {
-    if (idx === nums.length) {
-      if (t === S) result++
-      return
-    }
-
-    dfs(idx + 1, t + nums[idx])
-    dfs(idx + 1, t - nums[idx])
+  const len = nums.length
+  const t = nums.reduce((a, b) => Math.abs(a) + Math.abs(b))
+  //动态规划
+  const dp = []
+  for (let i = 0; i <= len; i++) {
+    dp[i] = Array(2 * t + 1).fill(0)
   }
+  if (Math.abs(S) > Math.abs(t)) return 0
+
+  dp[0][0 + t] = 1
+
+  for (let i = 1; i <= len; i++) {
+    const c = nums[i - 1]
+    for (let j = -t; j <= t; j++) {
+      if (j - c + t >= 0) {
+        dp[i][j + t] += dp[i - 1][j - c + t]
+      }
+      if (j + c + t <= 2 * t) {
+        dp[i][j + t] += dp[i - 1][j + c + t]
+      }
+    }
+  }
+  //console.log('dp', dp)
+  return dp[len][S + t]
 }
 
-var nums = [1, 1, 1, 1, 1],
+var nums = [1],
   target = 3
 var aa = findTargetSumWays(nums, target)
 console.log(aa)

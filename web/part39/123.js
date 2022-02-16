e275211d3c2d1a3329e0942d8bf4ad02532282b3
北斗 <1912128546@qq.com>
2022-02-16 22:24:29 +0800

    ✨ feat:123. 买卖股票的最佳时机 III

---
+++ web/part39/123.js
@@ -0,0 +1,14 @@
+var maxProfit = function (prices) {
+  // 动态规划
+  const len = prices.length
+  const dp = [0, 0, 0, 0, 0]
+  dp[1] = -prices[0]
+  dp[3] = -prices[0]
+  for (let i = 1; i < len; i++) {
+    dp[1] = Math.max(dp[1], dp[0] - prices[i])
+    dp[2] = Math.max(dp[2], dp[1] + prices[i])
+    dp[3] = Math.max(dp[3], dp[2] - prices[i])
+    dp[4] = Math.max(dp[4], dp[3] + prices[i])
+  }
+  return dp[4]
+}

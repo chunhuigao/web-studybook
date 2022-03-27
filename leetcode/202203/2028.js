208e687af6095851d2be54d804ddf50eecee4653
北斗 <1912128546@qq.com>
2022-03-27 19:12:36 +0800

    ✨ feat:2028. 找出缺失的观测数据

---
+++ leetcode/202203/2028.js
@@ -0,0 +1,18 @@
+var missingRolls = function (rolls, mean, n) {
+  const m = rolls.length;
+  const sum = mean * (n + m);
+  let missingSum = sum;
+  for (const roll of rolls) {
+    missingSum -= roll;
+  }
+  if (missingSum < n || missingSum > 6 * n) {
+    return [];
+  }
+  const quotient = Math.floor(missingSum / n),
+    remainder = missingSum % n;
+  const missing = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    missing[i] = quotient + (i < remainder ? 1 : 0);
+  }
+  return missing;
+};

d4570fe46dd7d550a3d9e6dfcc22327a85178f67
北斗 <1912128546@qq.com>
2022-04-07 22:09:21 +0800

    ✨ feat:安装shelljs

--- push.js
+++ push.js
@@ -1,20 +1,20 @@
-var shell = require('shelljs')
+var shell = require('shelljs');
 //判定git命令是否可用
 if (!shell.which('git')) {
   //向命令行打印git命令不可用的提示信息
-  shell.echo('Sorry, this script requires git')
+  shell.echo('Sorry, this script requires git');
   //退出当前进程
-  shell.exit(1)
+  shell.exit(1);
 }
 
 // shell.exec('git add index.js')
 // shell.exec('git commit -m "使用nodejs自动执行git"')
 function start() {
-  var code = shell.exec('git push origin master').code
+  var code = shell.exec('git push origin main').code;
   if (code === 0) {
-    shell.exit(1)
+    shell.exit(1);
   } else {
-    setTimeout(start, 1000)
+    setTimeout(start, 1000);
   }
 }
-start()
+start();

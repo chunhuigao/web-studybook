02c53e6f27f4eaa7ba2a939417ad99fe9467c2a9
北斗 <1912128546@qq.com>
2022-05-18 21:32:15 +0800

    ✨ feat:668. 乘法表中第k小的数

---
+++ leetcode/202205/668.js
@@ -0,0 +1,17 @@
+var findKthNumber = function (m, n, k) {
+  let left = 1,
+    right = m * n;
+  while (left < right) {
+    const x = left + Math.floor((right - left) / 2);
+    let count = Math.floor(x / n) * n;
+    for (let i = Math.floor(x / n) + 1; i <= m; ++i) {
+      count += Math.floor(x / i);
+    }
+    if (count >= k) {
+      right = x;
+    } else {
+      left = x + 1;
+    }
+  }
+  return left;
+};

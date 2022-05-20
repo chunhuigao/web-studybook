071709a0c2fb53391d4b2ddaae82e81282141e72
北斗 <1912128546@qq.com>
2022-05-20 23:06:27 +0800

    ✨ feat:436. 寻找右区间

---
+++ leetcode/202205/436.js
@@ -0,0 +1,27 @@
+var findRightInterval = function (intervals) {
+  const n = intervals.length;
+  const startIntervals = new Array(n).fill(0).map(() => new Array(2).fill(0));
+  for (let i = 0; i < n; i++) {
+    startIntervals[i][0] = intervals[i][0];
+    startIntervals[i][1] = i;
+  }
+  startIntervals.sort((o1, o2) => o1[0] - o2[0]);
+
+  const ans = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    let left = 0;
+    let right = n - 1;
+    let target = -1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      if (startIntervals[mid][0] >= intervals[i][1]) {
+        target = startIntervals[mid][1];
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    ans[i] = target;
+  }
+  return ans;
+};

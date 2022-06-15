aed44ba55d85496b81d185939e81789e606e7547
北斗 <1912128546@qq.com>
2022-06-15 21:36:29 +0800

    ✨ feat:719. 找出第 K 小的数对距离

---
+++ leetcode/202206/719.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestDistancePair = function (nums, k) {
+  nums.sort((a, b) => a - b);
+  let n = nums.length,
+    left = 0,
+    right = nums[n - 1] - nums[0];
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    let cnt = 0;
+    for (let j = 0; j < n; j++) {
+      const i = binarySearch(nums, j, nums[j] - mid);
+      cnt += j - i;
+    }
+    if (cnt >= k) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
+
+const binarySearch = (nums, end, target) => {
+  let left = 0,
+    right = end;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};

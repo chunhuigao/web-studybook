b679f233009fa19c47fb5499fba4b264a6e1d85f
北斗 <1912128546@qq.com>
2022-06-29 22:42:25 +0800

    ✨ feat:535. TinyURL 的加密与解密

---
+++ leetcode/202206/535.js
@@ -0,0 +1,13 @@
+var encode = function (longUrl) {
+  return longUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function (shortUrl) {
+  return shortUrl;
+};

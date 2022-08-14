568bff748f874fbc1fda1b124445782e04a710be
北斗 <1912128546@qq.com>
2022-08-14 11:38:54 +0800

    1422. 分割字符串的最大得分

---
+++ leetcode/202208/1422.js
@@ -0,0 +1,19 @@
+var maxScore = function (s) {
+  let ans = 0;
+  const n = s.length;
+  for (let i = 1; i < n; i++) {
+    let score = 0;
+    for (let j = 0; j < i; j++) {
+      if (s[j] === '0') {
+        score++;
+      }
+    }
+    for (let j = i; j < n; j++) {
+      if (s[j] === '1') {
+        score++;
+      }
+    }
+    ans = Math.max(ans, score);
+  }
+  return ans;
+};
